d24081097474034bbd0f4045772c4eab9093d64d
Daniel Rodriguez 18896/6 <rodriguezd028@gmail.com>
2022-03-22 15:47:28 -0400

    Solucion actualizada

--- Desafios clase 2/clase2desafio3.py
+++ Desafios clase 2/clase2desafio3.py
@@ -1,13 +1,15 @@
 lista_de_notas = []
 nota = int(input("Ingresá una nota (-1 para finalizar)"))
 promedio = 0
-cantidad = 0
 while nota != -1:
     lista_de_notas.append(nota)
-    cantidad+=1
     promedio+=nota
     nota = int(input("Ingresá una nota (-1 para finalizar)"))
-promedio/=cantidad
-for i in lista_de_notas:
-    if i<promedio:
-        print(f'LA NOTA {i} ES MENOR AL PROMEDIO {promedio}')
+cant=len(lista_de_notas)
+if cant != 0:
+    promedio/= cant
+    for i in lista_de_notas:
+        if i<promedio:
+            print(f'LA NOTA {i} ES MENOR AL PROMEDIO {promedio}')
+else:
+    print('No ingresaste ningun numero')
