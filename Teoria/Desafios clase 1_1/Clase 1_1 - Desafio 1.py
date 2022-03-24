30ed4b7914a42b0a6b065761fb2b873da3f024b2
Daniel Rodriguez 18896/6 <rodriguezd028@gmail.com>
2022-03-24 17:00:30 -0400

    Detectar si una palabra tiene R

--- Teoria/Desafios clase 1_1/Clase 1_1 - Desafio 1.py
+++ Teoria/Desafios clase 1_1/Clase 1_1 - Desafio 1.py
@@ -1,7 +1,9 @@
 palabras_con_r = []
+
 for i in range(4):
     palabra = input("Ingrese una palabra: ")
     tiene_r = False
+    #Si solo una letra de la palabra tiene R, tiene_r se hace verdadero
     for k in palabra:
         if k=='r' or k=='R':
             tiene_r = True
