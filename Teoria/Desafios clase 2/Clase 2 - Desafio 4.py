a39108e0acd03ee43e372499b2f25ca4fcb4c0fb
Daniel Rodriguez 18896/6 <rodriguezd028@gmail.com>
2022-03-27 01:33:19 -0400

    Solucionado el problema de division por 0

--- Teoria/Desafios clase 2/Clase 2 - Desafio 4.py
+++ Teoria/Desafios clase 2/Clase 2 - Desafio 4.py
@@ -8,19 +8,19 @@ def ingresar_notas():
         nombre = input("Ingresa un nombre: ")
     return alumnos
 
-def calcular_promedio(t):
+def calcular_promedio(diccionario_alumnos):
     suma = 0
-    for i in t:
-        suma += t[i]
-    return suma/len(t)
+    for i in diccionario_alumnos:
+        suma += diccionario_alumnos[i]
+    return suma/len(diccionario_alumnos) if len(diccionario_alumnos) else 0
 
-def imprimir_menores(t,p):
-    for i in t:
-        if t[i] < p:
-            print(f"{i} tiene de nota {t[i]}, la cual es menor al promedio de {p}")
+def imprimir_menores(diccionario_alumnos,p):
+    for i in diccionario_alumnos:
+        if diccionario_alumnos[i] < p:
+            print(f"{i} tiene de nota {diccionario_alumnos[i]}, la cual es menor al promedio de {p}")
 
 todos_los_alumnos = ingresar_notas()
 promedio = calcular_promedio(todos_los_alumnos)
 
-print("Lista de alumnos con nota menor al promedio: \n\n")
+print("Lista de alumnos con nota menor al promedio: \n")
 imprimir_menores(todos_los_alumnos,promedio)
