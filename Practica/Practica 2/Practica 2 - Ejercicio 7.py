3a338abb6457aa1ff7e71c7712170141466b1160
Daniel Rodriguez 18896/6 <rodriguezd028@gmail.com>
2022-04-09 03:45:58 -0400

    Corregido error con el ejercicio 7 de la practica 2

--- Practica/Practica 2/Practica 2 - Ejercicio 7.py
+++ Practica/Practica 2/Practica 2 - Ejercicio 7.py
@@ -1,6 +1,18 @@
+import string
+
 devolver_conjunto = lambda frase_repetida: set(frase_repetida.lower())
 
 print('Ingrese una frase')
 frase = input()
 
-print(f'{frase} es un heterograma' if len(frase) == len(devolver_conjunto(frase)) else f'{frase} NO es un heterograma')
+#Estas lineas cuentan los espacios, si no lo hicieran tendriamos un problema
+#por ejemplo, la frase 'dia ron gemus' no seria un heterograma porque tiene 2 espacios
+espacios = 0
+primer_espacio = True
+for i in range(len(frase)):
+    if frase[i] == ' ' and primer_espacio:
+        primer_espacio = False
+    elif frase[i] == ' ' and not primer_espacio:
+        espacios +=1
+
+print(f'{frase} es un heterograma' if (len(frase) - espacios) == len(devolver_conjunto(frase)) else f'{frase} NO es un heterograma')
