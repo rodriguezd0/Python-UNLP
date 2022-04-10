0ab4cb15dc59ede2a892ef089ff3163c52333653
Daniel Rodriguez 18896/6 <rodriguezd028@gmail.com>
2022-04-10 11:54:36 -0400

    Terminado el ejercicio 10 de la practica 2

--- Practica/Practica 2/Practica 2 - Ejercicio 10.py
+++ Practica/Practica 2/Practica 2 - Ejercicio 10.py
@@ -1,34 +1,42 @@
 #Fase de lectura de archivos
-nombres = open('nombres_1.txt', 'r').read()
-eval_1 = open('eval1.txt','r').read()
-eval_2 = open('eval2.txt','r').read()
+def obtener_datos():
+    """ Obtengo los datos de los archivos y los guardo en listas
+    """
+    nombres = open('nombres_1.txt', 'r').read()
+    eval_1 = open('eval1.txt','r').read()
+    eval_2 = open('eval2.txt','r').read()
 
-#Fase de modificacion de archivos
-nombres = nombres.replace('\'','').replace('\n','').replace(' ','')
-eval_1 = eval_1.strip('\n ')
-eval_2 = eval_2.strip('\n ')
+    #Fase de modificacion de archivos
+    nombres = nombres.replace('\'','').replace('\n','').replace(' ','')
+    eval_1 = eval_1.strip('\n ')
+    eval_2 = eval_2.strip('\n ')
 
-nombres = nombres.split(',')
-eval_1 = eval_1.split(',')
-eval_2 = eval_2.split(',')
+    nombres = nombres.split(',')
+    eval_1 = eval_1.split(',')
+    eval_2 = eval_2.split(',')
+    return nombres,eval_1,eval_2
 
 #Fase donde se crea la tabla
-def crear_tabla():
+def crear_tabla(nombres,eval_1,eval_2):
+    """Ingreso todos los datos a una matriz y mientras los ingreso calculo el promedio"""
     tabla = []
     promedio = 0
     for i in range(len(nombres)):
         actual = []
         actual.append(nombres[i])
-        actual.append(int(eval_1[i]))
-        actual.append(int(eval_2[i]))
         actual.append(int(eval_1[i])+int(eval_2[i]))
         tabla.append(actual)
         promedio+=int(eval_1[i])+int(eval_2[i])
     promedio/=len(nombres)
     return promedio,tabla
 
-#Fase donde se imprime la tabla
-promedio,tabla = crear_tabla()
-print(f'{"ID":>5} {"NOMBRE":<20} {"NOTA 1":<8} {"NOTA 2":<8} {"TOTAL":<8} PROMEDIO({promedio:<8})')
-for i in range(len(tabla)):
-    print(f'{i:>5} {tabla[i][0]:<20} {tabla[i][1]:<8} {tabla[i][2]:<8} {tabla[i][3]:<8}',f'{"IGUAL O POR ENCIMA DEL PROMEDIO":<8}' if tabla[i][3] >= promedio else f'{"POR DEBAJO DEL PROMEDIO":<8}')
+def imprimir_datos(promedio,tabla):
+    """Recorro toda la tabla"""
+    print(f'{"ID":>5} {"NOMBRE":<20} {"TOTAL":<8} PROMEDIO({promedio:<8})')
+    for i in range(len(tabla)):
+        print(f'{i:>5} {tabla[i][0]:<20} {tabla[i][1]:<8}',f'{"IGUAL O POR ENCIMA DEL PROMEDIO":<8}' if tabla[i][1] >= promedio else f'{"POR DEBAJO DEL PROMEDIO":<8}')
+
+
+nombres,eval1,eval2 = obtener_datos()
+promedio,tabla = crear_tabla(nombres,eval1,eval2)
+imprimir_datos(promedio,tabla)
