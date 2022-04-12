a8408f239870ed46f3543f7c66ab638f614429d3
Daniel Rodriguez 18896/6 <rodriguezd028@gmail.com>
2022-04-12 15:44:22 -0400

    Ahora usa el metodo with

--- Teoria/Desafios clase 4/Clase 4- Desafio 1.py
+++ Teoria/Desafios clase 4/Clase 4- Desafio 1.py
@@ -3,30 +3,31 @@ import collections
 from collections import Counter
 
 
-def crear_archivos():
+def abrir_archivos():
     ruta = 'netflix_titles.csv'
     nuevo_archivo = 'paises del anio 2021.csv'
 
     catalogo = open(ruta,'r')
-    leer_catalogo = csv.reader(catalogo,delimiter=',')
-
     nuevo_catalogo = open(nuevo_archivo,'w')
-    escribir_catalogo = csv.writer(nuevo_catalogo)
 
-    return leer_catalogo,escribir_catalogo
+    return catalogo,nuevo_catalogo
 
 
-def procesar_archivo(leer_catalogo):
+def procesar_archivo(catalogo):
     paises = {}
-    escribir_catalogo.writerow(next(leer_catalogo))
-    for i in leer_catalogo:
-        if i[5] in paises:
-            paises[i[5]]+=1
-        else:
-             paises[i[5]] = 1
-        if i[7] == '2021':
-            escribir_catalogo.writerow(i)
-    print('Catalogo de las peliculas del anio 2021 creado correctamente.')
+    with catalogo as salida:
+        leer_catalogo = csv.reader(salida,delimiter=',')
+        with nuevo_catalogo as entrada:
+            escribir_catalogo = csv.writer(entrada)
+            escribir_catalogo.writerow(next(leer_catalogo))
+            for i in leer_catalogo:
+                if i[5] in paises:
+                    paises[i[5]]+=1
+                else:
+                     paises[i[5]] = 1
+                if i[7] == '2021':
+                    escribir_catalogo.writerow(i)
+            print('Catalogo de las peliculas del anio 2021 creado correctamente.')
     return paises,escribir_catalogo
 
 
@@ -36,7 +37,6 @@ def imprimir_mas_comunes(paises):
         print(f'{i+1}) {mas_comun[i][0]} con {mas_comun[i][1]} producciones')
 
 
-leer_catalogo,escribir_catalogo = crear_archivos()
-paises,escribir_catalogo = procesar_archivo(leer_catalogo)
+catalogo,nuevo_catalogo = abrir_archivos()
+paises,escribir_catalogo = procesar_archivo(catalogo)
 imprimir_mas_comunes(paises)
-nuevo_catalogo.close()
