8e12c0186291e13e46d92b3f48666c60d286b567
Daniel Rodriguez 18896/6 <rodriguezd028@gmail.com>
2022-04-12 14:23:02 -0400

    Desafio 1 de la clase 4 terminado

--- Teoria/Desafios clase 4/Clase 4 - Desafio 4.py
+++ Teoria/Desafios clase 4/Clase 4 - Desafio 4.py
@@ -3,17 +3,29 @@ import collections
 from collections import Counter
 
 ruta = 'netflix_titles.csv'
+nuevo_archivo = 'paises del anio 2021.csv'
+
 catalogo = open(ruta,'r')
 leer_catalogo = csv.reader(catalogo,delimiter=',')
+
+nuevo_catalogo = open(nuevo_archivo,'w')
+escribir_catalogo = csv.writer(nuevo_catalogo)
+
 paises = {}
+
+escribir_catalogo.writerow(next(leer_catalogo))
 
 for i in leer_catalogo:
     if i[5] in paises:
         paises[i[5]]+=1
     else:
          paises[i[5]] = 1
+    if i[7] == '2021':
+        escribir_catalogo.writerow(i)
 
 mas_comun = Counter(paises).most_common(5)
 
 for i in range(len(mas_comun)):
     print(f'{i+1}) {mas_comun[i][0]} con {mas_comun[i][1]} producciones')
+
+nuevo_catalogo.close()
