82db07e212d615e0af1f1e732244678b412fb7f1
Daniel Rodriguez 18896/6 <rodriguezd028@gmail.com>
2022-04-12 01:38:49 -0400

    Desafio 5 terminado

--- Teoria/Desafios clase 3/Clase 3 - Desafio 5.py
+++ Teoria/Desafios clase 3/Clase 3 - Desafio 5.py
@@ -1,5 +1,7 @@
 import string
 encriptar_letra = lambda x: chr(ord(x)+1) if x not in 'zZ' else chr(ord(x)-25)
 encriptar_texto = lambda x: list(map(encriptar_letra,x))
+unir_texto = lambda x: ''.join(x)
 texto_a_encriptar = input('Ingrese el texto a encriptar: ')
-print(encriptar_texto(texto_a_encriptar))
+texto_a_encriptar = encriptar_texto(texto_a_encriptar)
+print(unir_texto(texto_a_encriptar))
